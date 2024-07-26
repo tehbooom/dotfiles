bf2f220c060e0979438d661b943f4487e31764c7
tehbooom <onlyboomm@gmail.com>
2024-07-26 14:47:04 -0400

    added relative numbers

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,8 +1,18 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    opts = {},
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.window = opts.window or {}
+      opts.window.auto_expand_width = true
+      opts.filesystem.filtered_items = {
         visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
@@ -15,13 +25,15 @@ return {
         never_show = {
           ".git",
         },
-      },
-    },
-    event_handlers = {
-      event = "neo_tree_buffer_enter",
-      handler = function()
-        vim.opt_local.relativenumber = true
-      end,
-    },
+      }
+
+      opts.event_handlers = opts.event_handlers or {}
+      table.insert(opts.event_handlers, {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
+      })
+    end,
   },
 }
