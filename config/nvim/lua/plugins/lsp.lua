7b5c56fa6752bdd6274a860321748028a8592f57
tehbooom <ajcarp29@gmail.com>
2025-01-21 15:19:55 -0500

    fix: Add black lsp for python3

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -132,6 +132,26 @@ return {
           --   "yml",
           -- },
         },
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  enabled = false,
+                },
+                pyflakes = {
+                  enabled = true,
+                },
+                mccabe = {
+                  enabled = true,
+                },
+                black = {
+                  enabled = true,
+                },
+              },
+            },
+          },
+        },
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
